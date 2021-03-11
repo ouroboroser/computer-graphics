4a93e3ef59f9025ce6358c2f30c47fca9b81fffa
ouroboros <evgeniyagorshchyk@gmail.com>
2021-03-11 23:25:59 +0200

    image(ppm): add reader

--- convectors/ppm.py
+++ convectors/ppm.py
@@ -8,6 +8,7 @@ class _PPM:
         with open(name, 'rb') as ppm_file:
             return [row for row in ppm_file.readlines()]
 
+
     def clean(self, input_img):
         output_img = []
         for row in input_img:
@@ -16,3 +17,20 @@ class _PPM:
             if ('#' in row) == True:
                 output_img.remove(row)
         return  output_img
+
+    def data(self, input_img):
+        new_format = ' '.join(input_img)
+        size = new_format.split()[1:3]
+        size = list(map(int, size))
+
+        pixels = new_format.split()[4:]
+        pixels = list(map(int, pixels))
+        chunks = [pixels[i:i + 3] for i in range(0, len(pixels), 3)]
+
+        image_data = _IMAGE(
+            size[0],
+            size[1],
+            chunks,
+        )
+
+        return image_data
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from convectors.ppm import _PPM
-from convectors.image import _IMAGE
-
 name = 'test-images/test.ppm'
 
 _ppm_converter = _PPM('test')
 _ppm_image = _ppm_converter.read(name)
 _ppm_image = _ppm_converter.clean(_ppm_image)
 
-#size = _ppm_converter.get_header(_ppm_image)
+image = _ppm_converter.data(_ppm_image)
 
-#print('size: ', size)
-
-for row in _ppm_image:
-    print(row)
+print(image.width)
+print(image.height)
+print(image.pixel_map)
 
